7f3ffeb84f10b5978f4b234ab621d34a867b9f4b
Felipe-builder <felipesvascon@gmail.com>
2021-12-20 11:40:12 -0300

    Campos extras no Serializador e Serializador de Erro

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 const formatosAceitos = require('./Serializador').formatosAceitos
+const SerializadorErro = require('./Serializador').SerializadorErro
 
 app.use(bodyParser.json())
 
@@ -46,11 +47,15 @@ app.use((erro, req, res, proximo) => {
         status = 406
     }
 
+    const serializador = new SerializadorErro(
+        res.getHeader('Content-Type')
+    )
+
     res.status(status)
 
     res.send(
-        JSON.stringify({
-            menssagem: erro.message,
+        serializador.serializar({
+            mensagem: erro.message,
             id: erro.idErro
         })
     )
