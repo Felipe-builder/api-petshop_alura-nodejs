f0f9683ae182585345d397aef8e7b1f93902c40a
Felipe-builder <felipesvascon@gmail.com>
2022-01-18 10:27:30 -0300

    POST na rotas.v2.js

--- api/rotas/fornecedores/rotas.v2.js
+++ api/rotas/fornecedores/rotas.v2.js
@@ -1,5 +1,6 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 const SerializadorFornecedor = require('../../Serializador').SerializadorFornecedor
 
 roteador.options('/', (req, res) => {
@@ -18,6 +19,23 @@ roteador.get('/', async (req, res) => {
     res.send(
         serializador.serializar(resultados)
     )
+})
+
+roteador.post('/', async (req, res, proximo) => {
+    try {
+        const dadosRecebidos = req.body
+        const fornecedor = new Fornecedor(dadosRecebidos)
+        await fornecedor.criar()
+        const serializador = new SerializadorFornecedor(
+            res.getHeader('Content-Type')
+        )
+        res.status(201)
+        res.send(
+            serializador.serializar(fornecedor)
+        )
+    } catch(erro) {
+        proximo(erro)
+    }
 })
 
 module.exports = roteador
